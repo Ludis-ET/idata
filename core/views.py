820bd4805d5525e7c6f828a5910af673bbd40f4e
Ludis-ET <leulsegedmelaku1020@gmail.com>
2023-12-23 09:16:07 +0300

    some changes on index

--- core/views.py
+++ core/views.py
@@ -1,14 +1,17 @@
 from django.shortcuts import render
 from .models import *
+from blog.models import Blog
 
 def index(request):
     i = Idata.objects.get(id=1)
     em = Email.objects.all()
     ph = Phone.objects.all()
+    blogs = Blog.objects.all()
     context ={
         'i':i,
         'em':em,
         'ph':ph,
+        'blogs':blogs,
     }
     return render(request,'index.html',context)
 
