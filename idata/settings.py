aba41fb6cd7ff4a84cf86c761111add8c15517c8
Ludis-ET <leulsegedmelaku1020@gmail.com>
2023-12-23 14:36:46 +0300

    dd

--- idata/settings.py
+++ idata/settings.py
@@ -69,23 +69,23 @@ WSGI_APPLICATION = 'idata.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'railway',
-        'USER': 'postgres',
-        'PASSWORD': 'fB-36a-43-aaFDG2G*D2BGD4BdE1bdGB',
-        'HOST': 'roundhouse.proxy.rlwy.net',
-        'PORT': '49858',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'railway',
+#         'USER': 'postgres',
+#         'PASSWORD': 'fB-36a-43-aaFDG2G*D2BGD4BdE1bdGB',
+#         'HOST': 'roundhouse.proxy.rlwy.net',
+#         'PORT': '49858',
+#     }
+# }
 
 
 # Password validation
