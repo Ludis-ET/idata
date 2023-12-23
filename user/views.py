68d5c849192c5db99a4752b8d6211c58f6c302dd
Ludis-ET <leulsegedmelaku1020@gmail.com>
2023-12-23 11:11:08 +0300

    last call

--- user/views.py
+++ user/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render
 from .models import *
+from core.models import *
 
 
 def staff(request):
-    return render(request,'staff.html')
+    i = Idata.objects.get(id=1)
+    em = Email.objects.all()
+    ph = Phone.objects.all()
+    staffs = Staff.objects.all()
+    context ={
+        'i':i,
+        'em':em,
+        'ph':ph,
+        'staffs':staffs,
+    }
+    return render(request,'staff.html',context)
