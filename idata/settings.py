ddf8ffcbb961a15f2b9cd2983591b902dc264262
Ludis-ET <leulsegedmelaku1020@gmail.com>
2023-12-23 11:15:20 +0300

    collecting

--- idata/settings.py
+++ idata/settings.py
@@ -112,6 +112,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = 'staticfiles/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static/'),
 ]
