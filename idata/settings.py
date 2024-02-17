31f11514a4abb45d29b1e0539e170f8688ba85b3
Ludis-et <leulsegedmelaku1020@gmail.com>
2024-02-17 11:01:53 +0300

    Update Django version and add whitenoise.

--- idata/settings.py
+++ idata/settings.py
@@ -31,6 +31,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'whitenoise.runserver_nostatic',
 
 
     'core',
@@ -47,6 +48,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
 ]
 
 ROOT_URLCONF = 'idata.urls'
